1cf5add9fc489703c17ce024b7f5c1f24e5b66b4
Jimpie1043 <fx.leclerc05@gmail.com>
2026-09-15 20:24:31 -0400

    Register Post model
    Uses ModelAdmin to configure list displays, filters, search fields, prepopulated fields, autocomplete fields, date hierarchy, ordering, and facet counts.

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'slug', 'author', 'publish', 'status']
+    list_filter = ['status', 'created', 'publish', 'author', 'updated']
+    search_fields = ['title', 'body']
+    prepopulated_fields = {'slug': ('title',)}
+    autocomplete_fields = ['author']
+    date_hierarchy = 'publish'
+    ordering = ['status', 'publish']
+    show_facets = admin.ShowFacets.ALWAYS
