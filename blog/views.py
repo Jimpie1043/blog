e8ab588e1043104c70dba0b9b6baf72d232a4467
Jimpie1043 <fx.leclerc05@gmail.com>
2026-09-21 08:56:04 -0400

    Add paginator exception handling.
    posts variables is now wrapped inside a try: except block.
    
    On except EmptyPage, return last page.
    
    On except PageNotAnInteger, return first page.

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,20 @@
 from django.shortcuts import get_object_or_404, render
-from django.core.paginator import Paginator
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from .models import Post
 
 def post_list(request):
     post_list = Post.published.all()
+    
     paginator = Paginator(post_list, 3)
     page_number = request.GET.get('page', 1)
-    posts = paginator.page(page_number)
+    
+    try:
+        posts = paginator.page(page_number)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+    except PageNotAnInteger:
+        posts=paginator.page(1)
+        
     return render(
         request,
         'blog/post/list.html',
