9279b8bf411d07d50c91734c628243bd78c7557b
Jimpie1043 <fx.leclerc05@gmail.com>
2026-09-23 19:52:50 -0400

    Add post_share view
    The post_share view takes the request and post_id as parameters.
    
    It then retrieves the post the user wants to share by its id.
    
    If POST request: encloses the POSTed form inside a variable, checks if the form is valid, and cleans the form data.
    
    If not POST request: encloses the empty form inside a variable and returns a render with: the request, the url route, the post and the form.

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import get_object_or_404, render
 from django.views.generic import ListView
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from .models import Post
+from .forms import EmailPostForm
 
 def post_list(request):
     post_list = Post.published.all()
@@ -44,3 +45,29 @@ def post_detail(request, year, month, day, post):
         'blog/post/detail.html',
         {'post': post}
     )
+    
+def post_share(request, post_id):
+    # Retrieve post by id
+    post = get_object_or_404(
+        Post,
+        id=post_id,
+        status=Post.Status.PUBLISHED
+    )
+    
+    if request.method == 'POST':
+        # Form was submitted
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            # Form fields passed validation
+            cd = form.cleaned_data
+            # ... send email
+    else:
+        form = EmailPostForm
+        return render(
+            request,
+            'blog/post/share.html',
+            {
+                'post': post,
+                'form': form
+            }
+        )
